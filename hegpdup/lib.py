a826e0faff5409020b1644bb0f24171a40c9a2f1
Olivier Birot <olvb@github.com>
2023-02-16 14:41:01 +0100

    Remove now unused intersection helper

--- hegpdup/lib.py
+++ hegpdup/lib.py
@@ -14,15 +14,6 @@ def returnUniq(thisList):
     return list(set(thisList))
 
 
-# Python program to illustrate the intersection
-# of two lists using set() method
-# https://www.geeksforgeeks.org/python-intersection-two-lists/
-
-
-def intersection(lst1, lst2):
-    return list(set(lst1) & set(lst2))
-
-
 # https://stackoverflow.com/questions/7828867/how-to-efficiently-compare-two-unordered-lists-not-sets-in-python
 # O(n): The Counter() method is best (if your objects are hashable):
 def compareCounter(s, t):
