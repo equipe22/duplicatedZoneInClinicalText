70c91539cc62b0b03715cdef01d2a95b3deb2083
Olivier Birot <olvb@github.com>
2023-02-25 19:52:23 +0100

    Add speed comparison test with difflib

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,3 +1,5 @@
+import difflib
+import itertools
 from pathlib import Path
 import timeit
 
@@ -9,6 +11,7 @@ _TEXT_FILE = Path(__file__).parent / "sample_text.txt"
 _FINGERPRINT_LENGTH = 10
 _ORF = 3
 _MIN_DUPLICATE_LENGTH = 10
+_NB_REPEATS = 10
 
 
 def _getSampleTexts():
@@ -50,11 +53,43 @@ def test_speed(treeBackend):
         for i, text in enumerate(texts):
             duplicateFinder.findDuplicates(f"D{i}", text)
 
-    time = timeit.timeit(run, number=10)
+    time = timeit.timeit(run, number=_NB_REPEATS)
     print(treeBackend.value, time)
+
+
+@pytest.mark.parametrize("treeBackend", TreeBackend)
+def test_faster_than_difflib(treeBackend):
+    """
+    Make sure we are at least 10 times faster than difflib
+    """
+    texts = _getSampleTexts()
+
+    def run():
+        fingerprintBuilder = FingerprintBuilder([_FINGERPRINT_LENGTH], _ORF)
+        duplicateFinder = DuplicateFinder(
+            fingerprintBuilder,
+            minDuplicateLength=_MIN_DUPLICATE_LENGTH,
+            treeBackend=treeBackend,
+        )
+        for i, text in enumerate(texts):
+            duplicateFinder.findDuplicates(f"D{i}", text)
+
+    def run_difflib():
+        for source_text, target_text in itertools.combinations(texts, 2):
+            matcher = difflib.SequenceMatcher(
+                a=source_text, b=target_text, autojunk=False
+            )
+            matcher.get_matching_blocks()
+
+    time = timeit.timeit(run, number=_NB_REPEATS)
+    time_difflib = timeit.timeit(run_difflib, number=_NB_REPEATS)
+    print(time, time_difflib)
+
+    assert time < (time_difflib / 10)
 
 
 if __name__ == "__main__":
     # launch with python3 -O tests/test_speed.py for best results
     for treeBackend in TreeBackend:
         test_speed(treeBackend)
+        test_faster_than_difflib(treeBackend)
